f45013b814ce5b62225f7f564351b783c8df0b01
Charon Wang <63513719+CharonWangg@users.noreply.github.com>
2021-12-17 17:31:04 -0500

    Update add_swint_config.py

--- charon_code/utils/add_swint_config.py
+++ charon_code/utils/add_swint_config.py
@@ -3,10 +3,10 @@ from detectron2.config import CfgNode as CN
 def add_swint_config(cfg):
     # SwinT backbone
     cfg.MODEL.SWINT = CN()
-    cfg.MODEL.SWINT.EMBED_DIM = 128
+    cfg.MODEL.SWINT.EMBED_DIM = 192
     cfg.MODEL.SWINT.OUT_FEATURES = ["stage2", "stage3", "stage4", "stage5"]
     cfg.MODEL.SWINT.DEPTHS = [ 2, 2, 18, 2 ]
-    cfg.MODEL.SWINT.NUM_HEADS = [ 4, 8, 16, 32 ]
+    cfg.MODEL.SWINT.NUM_HEADS = [ 6, 12, 24, 48 ]
     cfg.MODEL.SWINT.WINDOW_SIZE = 12
     cfg.MODEL.SWINT.MLP_RATIO = 4
     cfg.MODEL.SWINT.DROP_PATH_RATE = 0.2
